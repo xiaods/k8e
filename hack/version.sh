82a72e8a99676bfd036fef30477198b6dfc278b6
Deshi Xiao <xiaods@gmail.com>
2020-11-18 22:20:13 +0800

    feat:add github action for tracking dev branch
    Signed-off-by: Deshi Xiao <xiaods@gmail.com>

---
+++ hack/version.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+GO=${GO-go}
+ARCH=${ARCH:-$("${GO}" env GOARCH)}
+SUFFIX="-${ARCH}"
+COMMIT=$(git rev-parse HEAD)
+
+VERSION_CONTAINERD=$(grep github.com/containerd/containerd go.mod | head -n1 | awk '{print $4}')
+if [ -z "$VERSION_CONTAINERD" ]; then
+    VERSION_CONTAINERD="v0.0.0"
+fi
+
+VERSION_CRICTL=$(grep github.com/kubernetes-sigs/cri-tools go.mod | head -n1 | awk '{print $4}')
+if [ -z "$VERSION_CRICTL" ]; then
+    VERSION_CRICTL="v0.0.0"
+fi
+
+VERSION_K8S=$(grep 'k8s.io/kubernetes v' go.mod | head -n1 | awk '{print $2}')
+if [ -z "$VERSION_K8S" ]; then
+    VERSION_K8S="v0.0.0"
+fi
+
+VERSION_CNIPLUGINS="v0.8.6-k3s1"
+
+
+VERSION="$VERSION_K8S+k8e-${COMMIT:0:8}$DIRTY"
+
+VERSION_TAG="$(sed -e 's/+/-/g' <<< "$VERSION")"
