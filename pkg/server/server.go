32ba75734f924249d1d95ac22cca59f9a8cfeeaa
Deshi Xiao <xiaods@gmail.com>
2021-02-09 14:37:17 +0800

    Update helm-controller to v0.8.3
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    
    cherry pick from k3s release-1.19 branch
    
    Signed-off-by: Deshi Xiao <xiaods@gmail.com>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -15,8 +15,8 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 
+	"github.com/k3s-io/helm-controller/pkg/helm"
 	"github.com/pkg/errors"
-	"github.com/rancher/helm-controller/pkg/helm"
 	v1 "github.com/rancher/wrangler-api/pkg/generated/controllers/core/v1"
 	"github.com/rancher/wrangler/pkg/leader"
 	"github.com/rancher/wrangler/pkg/resolvehome"
