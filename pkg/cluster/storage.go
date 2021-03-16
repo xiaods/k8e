f1de5c9e12578fd00ef29682db0cf249a5903bce
Deshi Xiao <xiaods@gmail.com>
2021-03-17 00:58:40 +0800

    porting: remove etcd data dir when etcd is disabled #3059
    Signed-off-by: Deshi Xiao <xiaods@gmail.com>

--- pkg/cluster/storage.go
+++ pkg/cluster/storage.go
@@ -3,8 +3,10 @@ package cluster
 import (
 	"bytes"
 	"context"
+	"strings"
 
 	"github.com/rancher/kine/pkg/client"
+	"github.com/sirupsen/logrus"
 	"github.com/xiaods/k8e/pkg/bootstrap"
 )
 
@@ -17,13 +19,26 @@ func (c *Cluster) save(ctx context.Context) error {
 	if err := bootstrap.Write(buf, &c.runtime.ControlRuntimeBootstrap); err != nil {
 		return err
 	}
-
 	data, err := encrypt(c.config.Token, buf.Bytes())
 	if err != nil {
 		return err
 	}
 
-	return c.storageClient.Create(ctx, storageKey(c.config.Token), data)
+	storageClient, err := client.New(c.etcdConfig)
+	if err != nil {
+		return err
+	}
+	if err := storageClient.Create(ctx, storageKey(c.config.Token), data); err != nil {
+		if err.Error() == "key exists" {
+			logrus.Warnln("Bootstrap key exists. Please follow documentation updating a node after restore.")
+			return nil
+		} else if strings.Contains(err.Error(), "not supported for learner") {
+			logrus.Debug("Skipping bootstrap data save on learner.")
+			return nil
+		}
+		return err
+	}
+	return nil
 }
 
 // storageBootstrap loads data from the datastore into the ControlRuntimeBootstrap struct.
