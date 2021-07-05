441e91ae590773556b2b3a6e419ae7e3d1f97bdd
Deshi Xiao <xiaods@gmail.com>
2021-07-06 00:20:40 +0800

    update cmm for k8e
    Signed-off-by: Deshi Xiao <xiaods@gmail.com>

--- pkg/cloudprovider/cloudprovider.go
+++ pkg/cloudprovider/cloudprovider.go
@@ -1,19 +1,22 @@
 package cloudprovider
 
 import (
-	"context"
 	"io"
 
-	"github.com/rancher/wrangler-api/pkg/generated/controllers/core"
-	coreclient "github.com/rancher/wrangler-api/pkg/generated/controllers/core/v1"
-	"github.com/rancher/wrangler/pkg/start"
 	"github.com/xiaods/k8e/pkg/version"
+	"k8s.io/client-go/informers"
+	informercorev1 "k8s.io/client-go/informers/core/v1"
+	"k8s.io/client-go/tools/cache"
 	cloudprovider "k8s.io/cloud-provider"
 )
 
 type k8e struct {
-	NodeCache coreclient.NodeCache
+	nodeInformer          informercorev1.NodeInformer
+	nodeInformerHasSynced cache.InformerSynced
 }
+
+var _ cloudprovider.Interface = &k8e{}
+var _ cloudprovider.InformerUser = &k8e{}
 
 func init() {
 	cloudprovider.RegisterCloudProvider(version.Program, func(config io.Reader) (cloudprovider.Interface, error) {
@@ -22,11 +25,11 @@ func init() {
 }
 
 func (k *k8e) Initialize(clientBuilder cloudprovider.ControllerClientBuilder, stop <-chan struct{}) {
-	coreFactory := core.NewFactoryFromConfigOrDie(clientBuilder.ConfigOrDie("cloud-controller-manager"))
+}
 
-	go start.All(context.Background(), 1, coreFactory)
-
-	k.NodeCache = coreFactory.Core().V1().Node().Cache()
+func (k *k8e) SetInformers(informerFactory informers.SharedInformerFactory) {
+	k.nodeInformer = informerFactory.Core().V1().Nodes()
+	k.nodeInformerHasSynced = k.nodeInformer.Informer().HasSynced
 }
 
 func (k *k8e) Instances() (cloudprovider.Instances, bool) {
@@ -58,5 +61,5 @@ func (k *k8e) ProviderName() string {
 }
 
 func (k *k8e) HasClusterID() bool {
-	return false
+	return true
 }
