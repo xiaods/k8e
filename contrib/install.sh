a44ad58755e650dd93bb7bc64271e047bea4d2d0
Deshi Xiao <xiaods@gmail.com>
2021-07-08 23:26:49 +0800

    setup easy install way to start k8e cluster
    Signed-off-by: Deshi Xiao <xiaods@gmail.com>

---
+++ contrib/install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+set -o noglob
+
+echo "Install... k8e binary to /opt/k8e"
+
+mkdir -p /opt/k8e && cd /opt/k8e
+
+curl -s https://api.github.com/repos/xiaods/k8e/releases/latest \
+| grep "browser_download_url.*k8e" \
+| cut -d '"' -f 4 \
+| wget -qi - &&  chmod +x k8e
+
+curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/start-bootstrap.sh -o start-bootstrap.sh
+
+curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/start-server.sh -o start-server.sh
+
+curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/start-agent.sh -o start-agent.sh
+
+curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/stop-k8e.sh -o stop-k8e.sh
+
+curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/setup-k8s-tools.sh -o setup-k8s-tools.sh
+
+curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/k8e-uninstall.sh -o k8e-uninstall.sh
+
+curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/setup-profile.sh -o setup-profile.sh
+
+echo "Done! Happy deployment."
+
+
--- contrib/start-agent.sh
+++ contrib/start-agent.sh
@@ -2,4 +2,4 @@
 
 echo "start agent"
 
-K8E_NODE_NAME=k8e-57 K8E_TOKEN=ilovek8e /opt/k8e/k8e agent --server https://172.25.1.55:6443 >> k8e.log 2>&1 &
+K8E_TOKEN=ilovek8e /opt/k8e/k8e agent --server https://172.25.1.55:6443 >> k8e.log 2>&1 &
