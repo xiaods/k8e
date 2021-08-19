d42953b4d5d0af66ad258ad1ffa2e1a011c872a4
Deshi Xiao <xiaods@gmail.com>
2021-08-19 12:45:27 +0800

    [Release-1.19] fix Node stuck at deletion
    Add a context timeout for the etcd remove peer function
    
    Signed-off-by: galal-hussein hussein.galal.ahmed.11@gmail.com
    Signed-off-by: Deshi Xiao <xiaods@gmail.com>

--- pkg/etcd/etcd.go
+++ pkg/etcd/etcd.go
@@ -53,7 +53,8 @@ const (
 	defaultKeepAliveTime    = 30 * time.Second
 	defaultKeepAliveTimeout = 10 * time.Second
 
-	maxBackupRetention = 5
+	maxBackupRetention   = 5
+	memberRemovalTimeout = time.Minute * 1
 )
 
 var (
@@ -561,6 +562,8 @@ func (e *ETCD) cluster(ctx context.Context, forceNew bool, options executor.Init
 
 // removePeer removes a peer from the cluster. The peer ID and IP address must both match.
 func (e *ETCD) removePeer(ctx context.Context, id, address string, removeSelf bool) error {
+	ctx, cancel := context.WithTimeout(ctx, memberRemovalTimeout)
+	defer cancel()
 	members, err := e.client.MemberList(ctx)
 	if err != nil {
 		return err
