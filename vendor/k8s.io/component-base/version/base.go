514341dbf186c7f8297c2bd711473ab4ffde7818
Deshi Xiao <xiaods@gmail.com>
2021-08-19 13:58:10 +0800

    Update Kubernetes to v1.19.14-k3s1
    Signed-off-by: Deshi Xiao <xiaods@gmail.com>

--- vendor/k8s.io/component-base/version/base.go
+++ vendor/k8s.io/component-base/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "19"
-	gitVersion   = "v1.19.12-k3s1"
-	gitCommit    = "3e32ea1663306f5e5b7f386fc190f0afefc80454"
+	gitVersion   = "v1.19.14-k3s1"
+	gitCommit    = "de864bbd93b6636951750854c6be3a9b07aa1fac"
 	gitTreeState = "clean"
-	buildDate = "2021-06-17T18:50:24Z"
+	buildDate = "2021-08-12T22:23:54Z"
 )
