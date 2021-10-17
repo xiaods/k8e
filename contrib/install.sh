3ddcc4195d2a68064015512a9dbbf3eb97f3a1a6
Deshi Xiao <xiaods@gmail.com>
2021-10-17 20:28:19 +0800

    update install add service file
    Signed-off-by: Deshi Xiao <xiaods@gmail.com>

--- contrib/install.sh
+++ contrib/install.sh
@@ -5,6 +5,14 @@ set -o noglob
 # ---use binary install directory
 BIN_DIR=/usr/local/bin
 PROFILE=~/.bashrc
+SYSTEM_NAME=k8e
+# --- use systemd directory if defined or create default ---
+SYSTEMD_DIR=/etc/systemd/system
+# --- set related files from system name ---
+SERVICE_K8E=${SYSTEM_NAME}.service
+FILE_K8E_SERVICE=${SYSTEMD_DIR}/${SERVICE_K8E}
+FILE_K8E_ENV=${SYSTEMD_DIR}/${SERVICE_K8E}.env
+SYSTEMD_TYPE=notify
 
 # --- helper functions for logs ---
 info()
@@ -54,48 +62,117 @@ create_symlinks() {
 
 # --- seutp profile ---
 source_profile() {
-if ! grep -s 'containerd\.sock' "$PROFILE"; then
-    echo 'export CONTAINERD_ADDRESS=/run/k8e/containerd/containerd.sock' >> "$PROFILE"
-fi
-if ! grep -s '\/usr\/local\/bin' "$PROFILE"; then
-    echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
-fi
-if ! grep -s 'docker=nerdctl' "$PROFILE"; then
-    echo 'alias docker=nerdctl' >> ~/.bashrc
-fi
+    if ! grep -s 'containerd\.sock' "$PROFILE"; then
+        $SUDO echo 'export CONTAINERD_ADDRESS=/run/k8e/containerd/containerd.sock' >> "$PROFILE"
+    fi
+    if ! grep -s '\/usr\/local\/bin' "$PROFILE"; then
+        $SUDO echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
+    fi
+    if ! grep -s 'docker=nerdctl' "$PROFILE"; then
+        $SUDO echo 'alias docker=nerdctl' >> ~/.bashrc
+    fi
+}
+
+# --- disable current service if loaded --
+systemd_disable() {
+    $SUDO systemctl disable ${SYSTEM_NAME} >/dev/null 2>&1 || true
+    $SUDO rm -f /etc/systemd/system/${SERVICE_K8E} || true
+    $SUDO rm -f /etc/systemd/system/${SERVICE_K8E}.env || true
+}
+
+# --- capture current env and create file containing k3s_ variables ---
+create_env_file() {
+    info "env: Creating environment file ${FILE_K8E_ENV}"
+    $SUDO touch ${FILE_K8E_ENV}
+    $SUDO chmod 0600 ${FILE_K8E_ENV}
+    env | grep '^K8E_' | $SUDO tee ${FILE_K8E_ENV} >/dev/null
+    env | grep '^CONTAINERD_' | $SUDO tee -a ${FILE_K8E_ENV} >/dev/null
+    env | grep -Ei '^(NO|HTTP|HTTPS)_PROXY' | $SUDO tee -a ${FILE_K8E_ENV} >/dev/null
+}
+
+# --- write systemd service file ---
+create_systemd_service_file() {
+    info "systemd: Creating service file ${FILE_K8E_SERVICE}"
+    $SUDO tee ${FILE_K8E_SERVICE} >/dev/null << EOF
+[Unit]
+Description=Simple Kubernetes Distribution
+Documentation=https://getk8e.com
+After=network-online.target
+Wants=network-online.target
+
+[Install]
+WantedBy=multi-user.target
+
+[Service]
+Type=${SYSTEMD_TYPE}
+EnvironmentFile=-/etc/default/%N
+EnvironmentFile=-/etc/sysconfig/%N
+EnvironmentFile=-${FILE_K8E_ENV}
+KillMode=process
+Delegate=yes
+# Having non-zero Limit*s causes performance problems due to accounting overhead
+# in the kernel. We recommend using cgroups to do container-local accounting.
+LimitNOFILE=1048576
+LimitNPROC=infinity
+LimitCORE=infinity
+TasksMax=infinity
+TimeoutStartSec=0
+Restart=always
+RestartSec=5s
+ExecStartPre=/bin/sh -xc '! /usr/bin/systemctl is-enabled --quiet nm-cloud-setup.service'
+ExecStartPre=-/sbin/modprobe br_netfilter
+ExecStartPre=-/sbin/modprobe overlay
+ExecStart=/usr/local/bin/k8e server --write-kubeconfig-mode 644
+EOF
 }
 
 # --- download k8e and setup all-in-one functions
 download_and_setup() {
 
-info "Install... k8e binary to ${BIN_DIR}"
+    # --- use /usr/local/bin if root can write to it, otherwise use /opt/bin if it exists
+    if ! $SUDO sh -c "touch ${BIN_DIR}/k8e-ro-test && rm -rf ${BIN_DIR}/k8e-ro-test"; then
+        if [ -d /opt/bin ]; then
+            BIN_DIR=/opt/bin
+        fi
+    fi
+ 
+    info "Install... k8e binary to ${BIN_DIR}"
+    cd $BIN_DIR
+    $SUDO curl -s https://api.github.com/repos/xiaods/k8e/releases/latest \
+        | grep "browser_download_url.*k8e" \
+        | cut -d '"' -f 4 \
+        | wget -qi - && \
+         $SUDO chmod +x k8e && \
+         $SUDO chmod 755 k8e && \
+         $SUDO chown root:root k8e
 
-cd $BIN_DIR
+    $SUDO curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/k8e-start-bootstrap.sh -o k8e-start-bootstrap.sh && \
+    $SUDO chmod +x k8e-start-bootstrap.sh
 
-curl -s https://api.github.com/repos/xiaods/k8e/releases/latest \
-| grep "browser_download_url.*k8e" \
-| cut -d '"' -f 4 \
-| wget -qi - &&  chmod +x k8e
+    $SUDO curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/k8e-start-server.sh -o k8e-start-server.sh && \
+    $SUDO  chmod +x k8e-start-server.sh
 
-curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/k8e-start-bootstrap.sh -o k8e-start-bootstrap.sh
+    $SUDO curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/k8e-start-agent.sh -o k8e-start-agent.sh &&  \
+    $SUDO chmod +x k8e-start-agent.sh
 
-curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/k8e-start-server.sh -o k8e-start-server.sh
+    $SUDO curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/k8e-stop.sh -o k8e-stop.sh && \
+    $SUDO  chmod +x k8e-stop.sh
 
-curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/k8e-start-agent.sh -o k8e-start-agent.sh
+    $SUDO curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/k8e-killall.sh -o k8e-killall.sh && \
+    $SUDO   chmod +x k8e-killall.sh
 
-curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/k8e-stop.sh -o k8e-stop.sh
-
-curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/k8e-killall.sh -o k8e-killall.sh
-
-curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/k8e-uninstall.sh -o k8e-uninstall.sh
+    $SUDO curl https://raw.githubusercontent.com/xiaods/k8e/master/contrib/k8e-uninstall.sh -o k8e-uninstall.sh && \
+    $SUDO  chmod +x k8e-uninstall.sh
 
 
-create_symlinks
-source_profile
+    create_symlinks
+    source_profile
+    systemd_disable
+    create_env_file
+    create_systemd_service_file
 
-$BIN_DIR/k8e check-config
-info "Done! Happy deployment."
-
+    $SUDO $BIN_DIR/k8e check-config
+    info "Done! Happy deployment."
 }
 
 # --- run the install process --
