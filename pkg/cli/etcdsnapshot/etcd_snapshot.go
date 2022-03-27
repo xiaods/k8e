01d450f8d28add4e88edac239b5ae6a8f952f5c5
Deshi Xiao <xiaods@gmail.com>
2022-03-27 14:39:26 +0800

    Move temporary etcd startup into etcd module
    Reuse the existing etcd library code to start up the temporary etcd
    server for bootstrap reconcile. This allows us to do proper
    health-checking of the datastore on startup, including handling of
    alarms.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    porting by
    Signed-off-by: Deshi Xiao <xiaods@gmail.com>

--- pkg/cli/etcdsnapshot/etcd_snapshot.go
+++ pkg/cli/etcdsnapshot/etcd_snapshot.go
@@ -87,7 +87,9 @@ func run(app *cli.Context, cfg *cmds.Server) error {
 
 	ctx := signals.SetupSignalHandler(context.Background())
 	e := etcd.NewETCD()
-	e.SetControlConfig(&serverConfig.ControlConfig)
+	if err := e.SetControlConfig(ctx, &serverConfig.ControlConfig); err != nil {
+		return err
+	}
 
 	initialized, err := e.IsInitialized(ctx, &serverConfig.ControlConfig)
 	if err != nil {
@@ -137,7 +139,9 @@ func delete(app *cli.Context, cfg *cmds.Server) error {
 
 	ctx := signals.SetupSignalHandler(context.Background())
 	e := etcd.NewETCD()
-	e.SetControlConfig(&serverConfig.ControlConfig)
+	if err := e.SetControlConfig(ctx, &serverConfig.ControlConfig); err != nil {
+		return err
+	}
 
 	sc, err := server.NewContext(ctx, serverConfig.ControlConfig.Runtime.KubeConfigAdmin)
 	if err != nil {
@@ -178,7 +182,9 @@ func list(app *cli.Context, cfg *cmds.Server) error {
 
 	ctx := signals.SetupSignalHandler(context.Background())
 	e := etcd.NewETCD()
-	e.SetControlConfig(&serverConfig.ControlConfig)
+	if err := e.SetControlConfig(ctx, &serverConfig.ControlConfig); err != nil {
+		return err
+	}
 
 	sf, err := e.ListSnapshots(ctx)
 	if err != nil {
@@ -245,7 +251,9 @@ func prune(app *cli.Context, cfg *cmds.Server) error {
 
 	ctx := signals.SetupSignalHandler(context.Background())
 	e := etcd.NewETCD()
-	e.SetControlConfig(&serverConfig.ControlConfig)
+	if err := e.SetControlConfig(ctx, &serverConfig.ControlConfig); err != nil {
+		return err
+	}
 
 	sc, err := server.NewContext(ctx, serverConfig.ControlConfig.Runtime.KubeConfigAdmin)
 	if err != nil {
