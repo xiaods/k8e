7d882e75c173ce47e832715c89990b09dc1b37ca
Deshi Xiao <xiaods@gmail.com>
2023-02-18 10:35:35 +0800

    Add certificate rotate-ca to write updated CA certs to datastore
    This command must be run on a server while the service is running. After
    this command completes, all the servers in the cluster should be
    restarted to load the new CA files.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    porting by
    Signed-off-by: Deshi Xiao <xiaods@gmail.com>

--- cmd/k8e/main.go
+++ cmd/k8e/main.go
@@ -70,7 +70,9 @@ func main() {
 		),
 		cmds.NewCertCommand(
 			cmds.NewCertSubcommands(
-				certCommand),
+				certCommand,
+				certCommand,
+			),
 		),
 		cmds.NewCompletionCommand(internalCLIAction(version.Program+"-completion", dataDir, os.Args)),
 	}
