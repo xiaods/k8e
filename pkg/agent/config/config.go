bc6ee46864b70199abd4b95210303ccc5d256f5f
Deshi Xiao <xiaods@gmail.com>
2023-11-18 08:58:14 +0800

    QoS-class resource configuration
    Problem:
    Configuring qos-class features in containerd requres a custom containerd configuration template.
    
    Solution:
    Look for configuration files in default locations and configure containerd to use them if they exist.
    
    Signed-off-by: Oliver Larsson <larsson.e.oliver@gmail.com>
    (cherry picked from commit 30c8ad9)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    porting by
    Signed-off-by: Deshi Xiao <xiaods@gmail.com>

--- pkg/agent/config/config.go
+++ pkg/agent/config/config.go
@@ -472,6 +472,7 @@ func get(ctx context.Context, envInfo *cmds.Agent, proxy proxy.Proxy) (*config.N
 	}
 	applyContainerdStateAndAddress(nodeConfig)
 	applyCRIDockerdAddress(nodeConfig)
+	applyContainerdQoSClassConfigFileIfPresent(envInfo, nodeConfig)
 	nodeConfig.Containerd.Template = filepath.Join(envInfo.DataDir, "agent", "etc", "containerd", "config.toml.tmpl")
 	nodeConfig.Certificate = servingCert
 
