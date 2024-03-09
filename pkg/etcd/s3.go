f4ea216fbd9ef8b5ff109522c59ee25037ab3405
Deshi Xiao <xiaods@gmail.com>
2024-03-09 13:29:57 +0800

    Don't request metadata when listing objects
    While some implementations may support it, it appears that most don't,
    and some may in fact return an error if it is requested.
    
    We already stat the object to get the metadata anyway, so this was
    unnecessary if harmless on most implementations.
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    porting by
    Signed-off-by: Deshi Xiao <xiaods@gmail.com>

--- pkg/etcd/s3.go
+++ pkg/etcd/s3.go
@@ -358,9 +358,8 @@ func (s *S3) listSnapshots(ctx context.Context) (map[string]snapshotFile, error)
 	defer cancel()
 
 	opts := minio.ListObjectsOptions{
-		Prefix:       s.config.EtcdS3Folder,
-		Recursive:    true,
-		WithMetadata: true,
+		Prefix:    s.config.EtcdS3Folder,
+		Recursive: true,
 	}
 
 	objects := s.client.ListObjects(ctx, s.config.EtcdS3BucketName, opts)
