7d6157e24e645c6786c1bd8f47bf4dfe4e731df9
Deshi Xiao <xiaods@gmail.com>
2024-11-20 13:22:37 +0800

    fix: have a try for defaultparser_test.go
    Signed-off-by: Deshi Xiao <xiaods@gmail.com>

--- pkg/configfilearg/defaultparser_test.go
+++ pkg/configfilearg/defaultparser_test.go
@@ -15,52 +15,52 @@ func Test_UnitMustParse(t *testing.T) {
 	}{
 		{
 			name: "Basic server",
-			args: []string{"k3s", "server"},
+			args: []string{"k8e", "server"},
 
-			want: []string{"k3s", "server"},
+			want: []string{"k8e", "server"},
 		},
 		{
 			name: "Server with known flags",
-			args: []string{"k3s", "server", "-t 12345", "--write-kubeconfig-mode 644"},
+			args: []string{"k8e", "server", "-t 12345", "--write-kubeconfig-mode 644"},
 
-			want: []string{"k3s", "server", "-t 12345", "--write-kubeconfig-mode 644"},
+			want: []string{"k8e", "server", "-t 12345", "--write-kubeconfig-mode 644"},
 		},
 		{
 			name:   "Server with known flags and config with known and unknown flags",
-			args:   []string{"k3s", "server", "--write-kubeconfig-mode 644"},
+			args:   []string{"k8e", "server", "--write-kubeconfig-mode 644"},
 			config: "./testdata/defaultdata.yaml",
-			want: []string{"k3s", "server", "--token=12345", "--node-label=DEAFBEEF",
+			want: []string{"k8e", "server", "--token=12345", "--node-label=DEAFBEEF",
 				"--etcd-s3=true", "--etcd-s3-bucket=my-backup", "--kubelet-arg=max-pods=999", "--write-kubeconfig-mode 644"},
 		},
 		{
 			name: "Basic etcd-snapshot",
-			args: []string{"k3s", "etcd-snapshot", "save"},
+			args: []string{"k8e", "etcd-snapshot", "save"},
 
-			want: []string{"k3s", "etcd-snapshot", "save"},
+			want: []string{"k8e", "etcd-snapshot", "save"},
 		},
 		{
 			name: "Etcd-snapshot with known flags",
-			args: []string{"k3s", "etcd-snapshot", "save", "--s3=true"},
+			args: []string{"k8e", "etcd-snapshot", "save", "--s3=true"},
 
-			want: []string{"k3s", "etcd-snapshot", "save", "--s3=true"},
+			want: []string{"k8e", "etcd-snapshot", "save", "--s3=true"},
 		},
 		{
 			name:   "Etcd-snapshot with config with known and unknown flags",
-			args:   []string{"k3s", "etcd-snapshot", "save"},
+			args:   []string{"k8e", "etcd-snapshot", "save"},
 			config: "./testdata/defaultdata.yaml",
-			want:   []string{"k3s", "etcd-snapshot", "save", "--etcd-s3=true", "--etcd-s3-bucket=my-backup"},
+			want:   []string{"k8e", "etcd-snapshot", "save", "--etcd-s3=true", "--etcd-s3-bucket=my-backup"},
 		},
 		{
 			name: "Agent with known flags",
-			args: []string{"k3s", "agent", "--token=12345"},
+			args: []string{"k8e", "agent", "--token=12345"},
 
-			want: []string{"k3s", "agent", "--token=12345"},
+			want: []string{"k8e", "agent", "--token=12345"},
 		},
 		{
 			name:   "Agent with config with known and unknown flags, flags are not skipped",
-			args:   []string{"k3s", "agent"},
+			args:   []string{"k8e", "agent"},
 			config: "./testdata/defaultdata.yaml",
-			want: []string{"k3s", "agent", "--token=12345", "--node-label=DEAFBEEF",
+			want: []string{"k8e", "agent", "--token=12345", "--node-label=DEAFBEEF",
 				"--etcd-s3=true", "--etcd-s3-bucket=my-backup", "--notaflag=true", "--kubelet-arg=max-pods=999"},
 		},
 	}
@@ -85,53 +85,53 @@ func Test_UnitMustFindString(t *testing.T) {
 	}{
 		{
 			name:   "Target not found in config file",
-			args:   []string{"k3s", "--foo", "bar"},
+			args:   []string{"k8e", "--foo", "bar"},
 			target: "token",
 
 			want: "",
 
-			setup:    func() error { return os.Setenv("K3S_CONFIG_FILE", "./testdata/data.yaml") },
-			teardown: func() error { return os.Unsetenv("K3S_CONFIG_FILE") },
+			setup:    func() error { return os.Setenv("K8E_CONFIG_FILE", "./testdata/data.yaml") },
+			teardown: func() error { return os.Unsetenv("K8E_CONFIG_FILE") },
 		},
 		{
 			name:   "Target found in config file",
-			args:   []string{"k3s", "--foo", "bar"},
+			args:   []string{"k8e", "--foo", "bar"},
 			target: "token",
 
 			want: "12345",
 
-			setup:    func() error { return os.Setenv("K3S_CONFIG_FILE", "./testdata/defaultdata.yaml") },
-			teardown: func() error { return os.Unsetenv("K3S_CONFIG_FILE") },
+			setup:    func() error { return os.Setenv("K8E_CONFIG_FILE", "./testdata/defaultdata.yaml") },
+			teardown: func() error { return os.Unsetenv("K8E_CONFIG_FILE") },
 		},
 		{
 			name:   "Override flag is returned if found",
-			args:   []string{"k3s", "--foo", "bar", "--version"},
+			args:   []string{"k8e", "--foo", "bar", "--version"},
 			target: "token",
 
 			want: "--version",
 
-			setup:    func() error { return os.Setenv("K3S_CONFIG_FILE", "./testdata/defaultdata.yaml") },
-			teardown: func() error { return os.Unsetenv("K3S_CONFIG_FILE") },
+			setup:    func() error { return os.Setenv("K8E_CONFIG_FILE", "./testdata/defaultdata.yaml") },
+			teardown: func() error { return os.Unsetenv("K8E_CONFIG_FILE") },
 		},
 		{
 			name:   "Override flag is not returned for specific subcommands",
-			args:   []string{"k3s", "ctr", "--foo", "bar", "--version"},
+			args:   []string{"k8e", "ctr", "--foo", "bar", "--version"},
 			target: "token",
 
 			want: "12345",
 
-			setup:    func() error { return os.Setenv("K3S_CONFIG_FILE", "./testdata/defaultdata.yaml") },
-			teardown: func() error { return os.Unsetenv("K3S_CONFIG_FILE") },
+			setup:    func() error { return os.Setenv("K8E_CONFIG_FILE", "./testdata/defaultdata.yaml") },
+			teardown: func() error { return os.Unsetenv("K8E_CONFIG_FILE") },
 		},
 		{
 			name:   "Override flag is not returned for specific subcommands with full path",
-			args:   []string{"/usr/local/bin/k3s", "ctr", "--foo", "bar", "--version"},
+			args:   []string{"/usr/local/bin/k8e", "ctr", "--foo", "bar", "--version"},
 			target: "token",
 
 			want: "12345",
 
-			setup:    func() error { return os.Setenv("K3S_CONFIG_FILE", "./testdata/defaultdata.yaml") },
-			teardown: func() error { return os.Unsetenv("K3S_CONFIG_FILE") },
+			setup:    func() error { return os.Setenv("K8E_CONFIG_FILE", "./testdata/defaultdata.yaml") },
+			teardown: func() error { return os.Unsetenv("K8E_CONFIG_FILE") },
 		},
 		{
 			name:   "Override flag is not returned for wrapped commands",
@@ -140,8 +140,8 @@ func Test_UnitMustFindString(t *testing.T) {
 
 			want: "12345",
 
-			setup:    func() error { return os.Setenv("K3S_CONFIG_FILE", "./testdata/defaultdata.yaml") },
-			teardown: func() error { return os.Unsetenv("K3S_CONFIG_FILE") },
+			setup:    func() error { return os.Setenv("K8E_CONFIG_FILE", "./testdata/defaultdata.yaml") },
+			teardown: func() error { return os.Unsetenv("K8E_CONFIG_FILE") },
 		},
 		{
 			name:   "Override flag is not returned for wrapped commands with full path",
@@ -150,8 +150,8 @@ func Test_UnitMustFindString(t *testing.T) {
 
 			want: "12345",
 
-			setup:    func() error { return os.Setenv("K3S_CONFIG_FILE", "./testdata/defaultdata.yaml") },
-			teardown: func() error { return os.Unsetenv("K3S_CONFIG_FILE") },
+			setup:    func() error { return os.Setenv("K8E_CONFIG_FILE", "./testdata/defaultdata.yaml") },
+			teardown: func() error { return os.Unsetenv("K8E_CONFIG_FILE") },
 		},
 	}
 	for _, tt := range tests {
