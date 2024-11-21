bd7dedf50411c8baaaaaf1544a76a29125fa9f08
Deshi Xiao <xiaods@gmail.com>
2024-11-21 15:12:03 +0800

    fix: update pkg/containerd pkg
    Signed-off-by: Deshi Xiao <xiaods@gmail.com>

--- pkg/containerd/builtins_linux.go
+++ pkg/containerd/builtins_linux.go
@@ -3,10 +3,13 @@
 
 /*
    Copyright The containerd Authors.
+
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
+
        http://www.apache.org/licenses/LICENSE-2.0
+
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,8 +21,9 @@ package containerd
 
 import (
 	_ "github.com/containerd/aufs/plugin"
+	_ "github.com/containerd/containerd/metrics"
 	_ "github.com/containerd/containerd/metrics/cgroups"
-	_ "github.com/containerd/containerd/runtime/v2"
+	_ "github.com/containerd/containerd/metrics/cgroups/v2"
 	_ "github.com/containerd/containerd/runtime/v2/runc/options"
 	_ "github.com/containerd/containerd/snapshots/btrfs/plugin"
 	_ "github.com/containerd/containerd/snapshots/devmapper/plugin"
