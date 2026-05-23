6d21e7fb425521c3203b7f0ccdfc6281d83417c1
Xiao Deshi <xiaods@gmail.com>
2026-05-23 15:48:33 +0800

    refactor: extract shared command funcs to eliminate 25 duplicated lines
    Move AppCommandFuncs initialization into pkg/cli/commands.Funcs(),
    shared by both main.go and cmd/server/main.go.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,17 +8,8 @@ import (
 
 	"github.com/docker/docker/pkg/reexec"
 	"github.com/sirupsen/logrus"
-	"github.com/xiaods/k8e/pkg/cli/agent"
-	"github.com/xiaods/k8e/pkg/cli/cert"
 	"github.com/xiaods/k8e/pkg/cli/cmds"
-	"github.com/xiaods/k8e/pkg/cli/completion"
-	"github.com/xiaods/k8e/pkg/cli/crictl"
-	"github.com/xiaods/k8e/pkg/cli/ctr"
-	"github.com/xiaods/k8e/pkg/cli/etcdsnapshot"
-	"github.com/xiaods/k8e/pkg/cli/kubectl"
-	"github.com/xiaods/k8e/pkg/cli/secretsencrypt"
-	"github.com/xiaods/k8e/pkg/cli/server"
-	"github.com/xiaods/k8e/pkg/cli/token"
+	"github.com/xiaods/k8e/pkg/cli/commands"
 	"github.com/xiaods/k8e/pkg/configfilearg"
 	"github.com/xiaods/k8e/pkg/containerd"
 	ctr2 "github.com/xiaods/k8e/pkg/ctr"
@@ -42,31 +33,7 @@ func main() {
 	os.Args[0] = cmd
 
 	app := cmds.NewApp()
-	app.Commands = cmds.NewAppCommands(cmds.AppCommandFuncs{
-		Server:  server.Run,
-		Agent:   agent.Run,
-		Kubectl: kubectl.Run,
-		CRICTL:  crictl.Run,
-		Ctr:     ctr.Run,
-		Token: cmds.TokenCommandFuncs{
-			Create: token.Create, Delete: token.Delete,
-			Generate: token.Generate, List: token.List, Rotate: token.Rotate,
-		},
-		EtcdSnapshot: cmds.EtcdSnapshotCommandFuncs{
-			Delete: etcdsnapshot.Delete, List: etcdsnapshot.List,
-			Prune: etcdsnapshot.Prune, Save: etcdsnapshot.Save,
-		},
-		SecretsEncrypt: cmds.SecretsEncryptCommandFuncs{
-			Status: secretsencrypt.Status, Enable: secretsencrypt.Enable,
-			Disable: secretsencrypt.Disable, Prepare: secretsencrypt.Prepare,
-			Rotate: secretsencrypt.Rotate, Reencrypt: secretsencrypt.Reencrypt,
-			RotateKeys: secretsencrypt.RotateKeys,
-		},
-		Cert: cmds.CertCommandFuncs{
-			Check: cert.Check, Rotate: cert.Rotate, RotateCA: cert.RotateCA,
-		},
-		Completion: completion.Run,
-	})
+	app.Commands = cmds.NewAppCommands(commands.Funcs())
 
 	if err := app.Run(configfilearg.MustParse(os.Args)); err != nil && !errors.Is(err, context.Canceled) {
 		logrus.Fatal(err)
--- main.go
+++ main.go
@@ -12,47 +12,14 @@ import (
 	"os"
 
 	"github.com/sirupsen/logrus"
-	"github.com/xiaods/k8e/pkg/cli/agent"
-	"github.com/xiaods/k8e/pkg/cli/cert"
 	"github.com/xiaods/k8e/pkg/cli/cmds"
-	"github.com/xiaods/k8e/pkg/cli/completion"
-	"github.com/xiaods/k8e/pkg/cli/crictl"
-	"github.com/xiaods/k8e/pkg/cli/ctr"
-	"github.com/xiaods/k8e/pkg/cli/etcdsnapshot"
-	"github.com/xiaods/k8e/pkg/cli/kubectl"
-	"github.com/xiaods/k8e/pkg/cli/secretsencrypt"
-	"github.com/xiaods/k8e/pkg/cli/server"
-	"github.com/xiaods/k8e/pkg/cli/token"
+	"github.com/xiaods/k8e/pkg/cli/commands"
 	"github.com/xiaods/k8e/pkg/configfilearg"
 )
 
 func main() {
 	app := cmds.NewApp()
-	app.Commands = cmds.NewAppCommands(cmds.AppCommandFuncs{
-		Server:  server.Run,
-		Agent:   agent.Run,
-		Kubectl: kubectl.Run,
-		CRICTL:  crictl.Run,
-		Ctr:     ctr.Run,
-		Token: cmds.TokenCommandFuncs{
-			Create: token.Create, Delete: token.Delete,
-			Generate: token.Generate, List: token.List, Rotate: token.Rotate,
-		},
-		EtcdSnapshot: cmds.EtcdSnapshotCommandFuncs{
-			Delete: etcdsnapshot.Delete, List: etcdsnapshot.List,
-			Prune: etcdsnapshot.Prune, Save: etcdsnapshot.Save,
-		},
-		SecretsEncrypt: cmds.SecretsEncryptCommandFuncs{
-			Status: secretsencrypt.Status, Enable: secretsencrypt.Enable,
-			Disable: secretsencrypt.Disable, Prepare: secretsencrypt.Prepare,
-			Rotate: secretsencrypt.Rotate, Reencrypt: secretsencrypt.Reencrypt,
-			RotateKeys: secretsencrypt.RotateKeys,
-		},
-		Cert: cmds.CertCommandFuncs{
-			Check: cert.Check, Rotate: cert.Rotate, RotateCA: cert.RotateCA,
-		},
-		Completion: completion.Run,
-	})
+	app.Commands = cmds.NewAppCommands(commands.Funcs())
 
 	if err := app.Run(configfilearg.MustParse(os.Args)); err != nil && !errors.Is(err, context.Canceled) {
 		logrus.Fatal(err)
